02c1d779ab7f8999e3c345f8ce1adc7ac5bf832d
matianfu <matianfu@gmail.com>
2014-06-24 09:18:35 +0800

    enable gpio init code, test passed.

--- Src/usart.c
+++ Src/usart.c
@@ -94,12 +94,12 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     PD5     ------> USART2_TX
     PD6     ------> USART2_RX 
     */
-//    GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6;
-//    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-//    GPIO_InitStruct.Pull = GPIO_NOPULL;
-//    GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
-//    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
-//    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
     /* Peripheral DMA init*/
   
