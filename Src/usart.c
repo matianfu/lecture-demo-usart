dda91f58a946cbf1bf4594d11bd4ecc2c1a3d955
matianfu <matianfu@gmail.com>
2014-06-25 14:26:00 +0800

    refactor gpio init code for usart3 inside mspinit.

--- Src/usart.c
+++ Src/usart.c
@@ -177,20 +177,7 @@ void MX_USART_UART_Init(UART_HandleTypeDef* huart)
 
 void MX_USART3_UART_Init(void)
 {
-
-//  huart3.Instance = USART3;
-//  huart3.Init.BaudRate = 115200;
-//  huart3.Init.WordLength = UART_WORDLENGTH_8B;
-//  huart3.Init.StopBits = UART_STOPBITS_1;
-//  huart3.Init.Parity = UART_PARITY_NONE;
-//  huart3.Init.Mode = UART_MODE_TX_RX;
-//  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-//  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
-
-#define huart3 	huartex3.huart	
-	
-  HAL_UART_Init(&huart3);
-
+  HAL_UART_Init(&huartex3.huart);
 }
 
 void HAL_UART_MspInit(UART_HandleTypeDef* huart)
@@ -242,13 +229,14 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     PD8     ------> USART3_TX
     PD9     ------> USART3_RX 
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART3;
-    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
-
+//    GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
+//    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+//    GPIO_InitStruct.Pull = GPIO_NOPULL;
+//    GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+//    GPIO_InitStruct.Alternate = GPIO_AF7_USART3;
+//    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+	
+		HAL_GPIO_Init(huartex->gpiox, huartex->gpio_init);
   }
 }
 
@@ -291,7 +279,7 @@ int unity_output_char(int a)
 {
 	uint8_t chr = a;
 	
-	HAL_UART_Transmit(&huart3, &chr, 1, 10);
+	HAL_UART_Transmit(&huartex3.huart, &chr, 1, 10);
 	return a;
 }
 
