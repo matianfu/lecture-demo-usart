fc2d10c6c6880f6c0c4b53e77b0b913c8af84497
matianfu <matianfu@gmail.com>
2014-06-26 16:37:03 +0800

    add dma test file.

---
+++ Test/dma_test.c
@@ -0,0 +1,86 @@
+#include <stdbool.h>
+#include "stm32f4xx_hal.h"
+#include "dma.h"
+#include "unity_fixture.h"
+
+/******************************************************************************
+
+analyze the problem
+
+OR-ing Switch Model
+
+1. There should be 8 switches for either DMA1 or DMA2;
+2. For either DMA, if 1 or more switch turn on, the clock should be on.
+3. For eiterh DMA, if no switch turned on, the clock should be off.
+
+Then Test Plan:
+1. clock off, turn A on, clock on.
+2. clock on, switch A on, turn A off, clock off.
+3. clock on, switch A on, switch B on, turn A off, clock on.
+
+******************************************************************************/
+
+static bool dma1_clock_enabled(void)
+{
+	return (RCC->AHB1ENR & RCC_AHB1ENR_DMA1EN) ? true : false;
+}
+
+TEST_GROUP(DMA_Clock);
+
+TEST_SETUP(DMA_Clock)
+{
+	__DMA1_CLK_DISABLE();
+	__DMA2_CLK_DISABLE();
+}
+
+TEST_TEAR_DOWN(DMA_Clock)
+{
+	__DMA1_CLK_ENABLE();
+	__DMA2_CLK_ENABLE();
+}
+
+/******************************************************************************
+#define __DMA1_CLK_ENABLE()          (RCC->AHB1ENR |= (RCC_AHB1ENR_DMA1EN))
+#define __DMA2_CLK_ENABLE()          (RCC->AHB1ENR |= (RCC_AHB1ENR_DMA2EN))
+******************************************************************************/
+TEST(DMA_Clock, ClockOffTurnOneOnClockOn)
+{
+	// assume all clocks off.
+	DMA_Clock_Get(DMA1_Stream5);
+	TEST_ASSERT_TRUE(dma1_clock_enabled());
+}
+
+TEST(DMA_Clock, ClockOffTurnOneOnBitSet)
+{
+	uint8_t dma1;
+	
+	DMA_Clock_Get(DMA1_Stream5);
+	DMA_Clock_Status(&dma1, 0);
+	TEST_ASSERT_TRUE(dma1 == (1<<5));
+}
+
+TEST(DMA_Clock, ClockOnTurnOffClockOff)
+{
+	DMA_Clock_Get(DMA1_Stream5);
+	DMA_Clock_Put(DMA1_Stream5);
+	TEST_ASSERT_FALSE(dma1_clock_enabled());
+}
+
+TEST(DMA_Clock, ClockOnTurnOffBitClear)
+{
+	uint8_t dma1;
+	
+	DMA_Clock_Get(DMA1_Stream5);
+	DMA_Clock_Put(DMA1_Stream5);
+	DMA_Clock_Status(&dma1, 0);
+	TEST_ASSERT_TRUE(dma1 == 0);
+}
+
+TEST_GROUP_RUNNER(DMA_Clock)
+{
+	RUN_TEST_CASE(DMA_Clock, ClockOffTurnOneOnClockOn);
+	RUN_TEST_CASE(DMA_Clock, ClockOffTurnOneOnBitSet);
+	RUN_TEST_CASE(DMA_Clock, ClockOnTurnOffClockOff);
+	RUN_TEST_CASE(DMA_Clock, ClockOnTurnOffBitClear);
+}
+
