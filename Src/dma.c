e32246a9a629909efe51c70961f21c8b5391a676
matianfu <matianfu@gmail.com>
2014-06-26 09:28:23 +0800

    remove dma irq enabling code in mx_dma_init()

--- Src/dma.c
+++ Src/dma.c
@@ -57,13 +57,13 @@ void MX_DMA_Init(void)
 
   /* DMA interrupt init */
   /* Sets the priority grouping field */
-  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
-  HAL_NVIC_SetPriority(DMA1_Stream6_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(DMA1_Stream6_IRQn);
+//  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
+//  HAL_NVIC_SetPriority(DMA1_Stream6_IRQn, 0, 0);
+//  HAL_NVIC_EnableIRQ(DMA1_Stream6_IRQn);
   /* Sets the priority grouping field */
-  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
-  HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
+//  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
+//  HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);
+//  HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
 }
 
 /* USER CODE BEGIN 2 */
